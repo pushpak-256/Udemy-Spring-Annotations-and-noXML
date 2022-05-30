6911063395b1a3110c268148cf750fc669ea69fa
Pushpak Bhagat <pbhagat@montran.com>
2022-05-30 20:15:48 +0530

    :package: Add Coach and FortuneService Interfaces

---
+++ javaConfig_noXML/com/pushpak/spring/coaches/Coach.java
@@ -0,0 +1,14 @@
+package com.pushpak.spring.coaches;
+
+/**
+ * 
+ * @author Pushpak B.
+ * <p>
+ * Common interface to create any type of Coach
+ * </p>
+ */
+public interface Coach
+{
+  public String getDailyWorkOut();
+  public String getDailyFortune();
+}
---
+++ javaConfig_noXML/com/pushpak/spring/fortunes/FortuneService.java
@@ -0,0 +1,13 @@
+package com.pushpak.spring.fortunes;
+
+/**
+ * 
+ * @author Pushpak B.
+ * <p>
+ * Common interface to create any type of Fortune
+ * </p>
+ */
+public interface FortuneService
+{
+  public String getDailyFortune();
+}
