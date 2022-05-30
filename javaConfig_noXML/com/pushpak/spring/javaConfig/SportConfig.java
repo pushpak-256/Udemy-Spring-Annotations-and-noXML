94269972ff8dee4251cd1ca0307199690284722b
Pushpak Bhagat <pbhagat@montran.com>
2022-05-30 20:08:14 +0530

    :wrench: Add Java Config file for Spring Container
    created new package and config file

---
+++ javaConfig_noXML/com/pushpak/spring/javaConfig/SportConfig.java
@@ -0,0 +1,19 @@
+package com.pushpak.spring.javaConfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * 
+ * This is Spring Config file with Java Code only
+ * 
+ * @author Pushpak Bhagat
+ */
+@Configuration
+@ComponentScan("com.pushpak.spring")
+public class SportConfig
+{
+   
+
+}
